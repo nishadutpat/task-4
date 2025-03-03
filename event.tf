13587a915695edc8c22fa9967bbb344ff2ed31be
nishad <nishad@gmail.com>
2025-03-03 11:38:12 +0530

    s3 bucket changed

--- event.tf
+++ event.tf
@@ -1,5 +1,6 @@
+
 resource "aws_s3_bucket_notification" "s3_trigger" {
-  bucket = aws_s3_bucket.buck-for-task4.id
+  bucket = aws_s3_bucket.buck-for-task-4.id
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.csv_lambda.arn
